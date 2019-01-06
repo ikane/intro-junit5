ed0f7d60a0b8c80e8bafb46891a9f50a1f9cad75
ikane <kaneelhadji2610@yahoo.fr>
2019-01-06 10:39:23 +0100

    experiment with @AfterEach, @BeforeAll and @AfterAll

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,13 +1,17 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     Greeting greeting;
+
+    @BeforeAll
+    public static void  beforeAllClass() {
+        System.out.println("In BeforeALL ********** ...");
+    }
 
     @BeforeEach
     void setUp() {
@@ -24,7 +28,16 @@ class GreetingTest {
 
     @Test
     void helloWorld1() {
-
         System.out.println(greeting.helloWorld("John"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In After each...");
+    }
+
+    @AfterAll
+    public static void  afterAllClass() {
+        System.out.println("In After ALL ********** ...");
     }
 }
